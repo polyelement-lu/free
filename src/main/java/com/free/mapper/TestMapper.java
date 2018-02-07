989a96ee5203261093e56c2ebd108c2c02cd0acc
polyelement-lu <ldm_yy@163.com>
2018-02-07 13:57:21 +0800

    q

--- src/main/java/com/free/mapper/TestMapper.java
+++
@@ -1,7 +0,0 @@
-package com.free.mapper;
-
-import com.free.common.dao.mapper.BaseMapper;
-import com.free.model.Test;
-
-public interface TestMapper extends BaseMapper<Test> {
-}
--- src/main/java/com/free/service/ITestService.java
+++
@@ -1,7 +0,0 @@
-package com.free.service;
-
-import com.free.common.BaseService;
-import com.free.model.Test;
-
-public interface ITestService extends BaseService<Test, String>{
-}
